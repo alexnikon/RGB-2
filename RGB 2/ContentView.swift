99ff9ee4511ecfca63e574099cc10767c4fe5b9c
Alex Nikonov <15732954+alexnikon@users.noreply.github.com>
2020-05-26 19:35:16 +0300

    Create sliders

--- RGB 2/ContentView.swift
+++ RGB 2/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center) {
+                VStack {
+                    Text("RED")
+                    Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/, in: -100...100, step: 0.1)
+                        .frame(width: 300)
+                        .accentColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+                        
+                        
+                }
+                VStack(alignment: .center) {
+                    Text("GREEN")
+                    Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/, in: -100...100, step: 0.1)
+                        .frame(width: 300.0)
+                        .accentColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
+                }
+                VStack(alignment: .center) {
+                Text("BLUE")
+                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/, in: -100...100, step: 0.1)
+                    .frame(width: 300)
+                    .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
 }
